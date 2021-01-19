81a74fe511f57ce1b485ea3e52c869d66ae0d7e3
lyh543 <lyh543@outlook.com>
2021-01-19 12:10:22 +0800

    修改 自动部署前进行测试x3

--- .github/workflows/django-deploy.sh
+++ .github/workflows/django-deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 cd /etc/uestcmsc_webapp/backend || return 1
+sudo systemctl stop uestcmsc_webapp_backend
 git pull
 python3 -m pip install --upgrade pip
 pip3 install -r requirements.txt
 python3 manage.py makemigrations accounts activities cloud gallery users
 python3 manage.py migrate
+sudo systemctl start uestcmsc_webapp_backend
