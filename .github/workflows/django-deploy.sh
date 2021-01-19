6bda4e2b55927cc27f52d7be899d6fddcb358f85
lyh543 <lyh543@outlook.com>
2021-01-19 11:37:45 +0800

    修改 自动部署前进行测试

--- .github/workflows/django-deploy.sh
+++ .github/workflows/django-deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-cd /etc/uestcmsc_webapp/backend || return 1
+set +x
+cd /etc/uestcmsc_webapp/backend || cd .
 git pull
+python3 -m pip install --upgrade pip
 pip3 install -r requirements.txt
-python3 manage.py makemigrations
+python3 manage.py makemigrations accounts activities cloud gallery users
 python3 manage.py migrate
