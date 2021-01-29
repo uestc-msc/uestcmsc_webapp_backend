0c9238a1be744777e2dfb993b1526ed24ef32bab
lyh543 <lyh543@outlook.com>
2021-01-29 19:58:14 +0800

    修复 static files 路径错误

--- .github/workflows/django-deploy.sh
+++ .github/workflows/django-deploy.sh
@@ -2,7 +2,7 @@
 
 cd /etc/uestcmsc_webapp/backend || return 1
 sudo systemctl stop uestcmsc_webapp_backend
-git pull
+git pull || return 1
 sudo python3 -m pip install --upgrade pip
 sudo pip3 install -r requirements.txt
 python3 manage.py makemigrations accounts activities cloud comment gallery users --noinput
