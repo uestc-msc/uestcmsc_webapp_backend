57cc4f28b5051ae0227dec225129c39b3feaabb6
lyh543 <lyh543@outlook.com>
2021-01-29 22:06:01 +0800

    一把键盘一个屏 一个配置写一天 x3

--- .github/workflows/django-deploy.sh
+++ .github/workflows/django-deploy.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-set -e  # 一旦出现错误就 exit
+set -xe
+
 cd /etc/uestcmsc_webapp/backend
 git pull
 sudo python3 -m pip install --upgrade pip
 sudo pip3 install -r requirements.txt
 python3 manage.py makemigrations accounts activities cloud comment gallery users --noinput
 python3 manage.py migrate --noinput
-python3 manage.py collectstatic
+python3 manage.py collectstatic --noinput --clear
 sudo systemctl restart uestcmsc_webapp_backend
 
 sudo cp deploy/Caddyfile /etc/caddy
 sudo systemctl restart caddy
+
+sleep 1
+curl -I "https://app.uestc-msc.com/" | grep "200"
+curl -I "https://app.uestc-msc.com/api/" | grep "200"
+curl -I "https://app.uestc-msc.com/api/static/" | grep "200"
