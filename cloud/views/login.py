4e04b682080286e9a47269e2df60d9c357638b1b
lyh543 <lyh543@outlook.com>
2021-04-26 16:00:30 +0800

    禁止非管理员登录云盘

--- cloud/views/login.py
+++ cloud/views/login.py
@@ -1,6 +1,6 @@
-
 from django.shortcuts import redirect
 from drf_yasg.utils import swagger_auto_schema
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -18,6 +18,8 @@ from utils.swagger import *
 )
 @api_view(['GET'])
 def onedrive_login(request: Request):
+    if not request.user.is_staff:
+        return Response(status=status.HTTP_403_FORBIDDEN)
     return redirect(OnedriveAuthentication.login_uri(), permanent=True)
 
 
